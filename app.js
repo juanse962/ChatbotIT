24eba9f8d38e50faa10d7c730fb6edb80ed39bef
Juan Gomez <gomex086@gmail.com>
2018-10-02 16:02:00 -0500

    Primer commit

---
+++ app.js
@@ -0,0 +1,113 @@
+var express = require('express');
+var request = require('request');
+var bodyParser = require('body-parser');
+var watson = require('watson-developer-cloud');
+var app = express();
+var contexid = "";
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+var conversation_id = "";
+var w_conversation = watson.conversation({
+    url: 'https://gateway.watsonplatform.net/conversation/api',
+    username: process.env.CONVERSATION_USERNAME || '8196b2be-6701-4beb-a2ba-e9f139cbfd43',
+    password: process.env.CONVERSATION_PASSWORD || 'raIguEqmuTDc',
+    version: 'v1',
+    version_date: '2016-07-11'
+});
+var workspace = process.env.WORKSPACE_ID || 'workspaceId';
+
+app.get('/webhook/', function(req, res) {
+    if (req.query['hub.verify_token'] === 'EAAD1FQ6JuxIBACrn7D7gz3S0az8Vmu9QuF4ZAq8SBqlxhYQCutc169ZCijhYziKgy3mdOzbTuBOU4APoRe3m4jNc8lSZBtbFrMrMaaHyrLQNIU8s7anZCgJT96IPBlfS0hAm59pMiozronkFjYMcYzswtCNFXEb8bWFbT7F3pwZDZD') {
+        res.send(req.query['hub.challenge']);
+    }
+    res.send('Erro de validação no token.');
+});
+
+app.post('/webhook/', function(req, res) {
+    var text = null;
+
+    messaging_events = req.body.entry[0].messaging;
+    for (i = 0; i < messaging_events.length; i++) {
+        event = req.body.entry[0].messaging[i];
+        sender = event.sender.id;
+
+        if (event.message && event.message.text) {
+            text = event.message.text;
+        } else if (event.postback && !text) {
+            text = event.postback.payload;
+        } else {
+            break;
+        }
+
+        var params = {
+            input: text,
+            // context: {"conversation_id": conversation_id}
+            context: contexid
+        }
+
+        var payload = {
+            workspace_id: "cf8b1b3f-c786-4e33-9616-ae6742fae3e5"
+        };
+
+        if (params) {
+            if (params.input) {
+                params.input = params.input.replace("\n", "");
+                payload.input = { "text": params.input };
+            }
+            if (params.context) {
+                payload.context = params.context;
+            }
+        }
+        callWatson(payload, sender);
+    }
+    res.sendStatus(200);
+});
+
+function callWatson(payload, sender) {
+    w_conversation.message(payload, function(err, convResults) {
+        console.log(convResults);
+        contexid = convResults.context;
+
+        if (err) {
+            return responseToRequest.send("Erro.");
+        }
+
+        if (convResults.context != null)
+            conversation_id = convResults.context.conversation_id;
+        if (convResults != null && convResults.output != null) {
+            var i = 0;
+            while (i < convResults.output.text.length) {
+                sendMessage(sender, convResults.output.text[i++]);
+            }
+        }
+
+    });
+}
+
+function sendMessage(sender, text_) {
+    text_ = text_.substring(0, 319);
+    messageData = { text: text_ };
+
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: { access_token: token },
+        method: 'POST',
+        json: {
+            recipient: { id: sender },
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending message: ', error);
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error);
+        }
+    });
+};
+
+var token = "EAAD1FQ6JuxIBACrn7D7gz3S0az8Vmu9QuF4ZAq8SBqlxhYQCutc169ZCijhYziKgy3mdOzbTuBOU4APoRe3m4jNc8lSZBtbFrMrMaaHyrLQNIU8s7anZCgJT96IPBlfS0hAm59pMiozronkFjYMcYzswtCNFXEb8bWFbT7F3pwZDZD";
+var host = (process.env.VCAP_APP_HOST || 'localhost');
+var port = (process.env.VCAP_APP_PORT || 3000);
+app.listen(port, host);
